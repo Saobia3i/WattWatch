fe9c33c4ca64e83fb99ec76c263c81508021dc28
Rafat Abdullah Pantho <rafatpantho@gmail.com>
2026-07-04 15:28:23 +0600

    fixed code and added diagram.

--- aurdino/sketch.ino
+++ aurdino/sketch.ino
@@ -113,7 +113,7 @@ void updateLoads() {
   bool isOccupied = (peopleCount > 0);
 
   // Note: digitalRead() == LOW means the slide switch is pulled to Ground (Flipped ON)
- bool l1State = (digitalRead(swLight1) == LOW);
+  bool l1State = (digitalRead(swLight1) == LOW);
   bool l2State = (digitalRead(swLight2) == LOW);
   bool l3State = (digitalRead(swLight3) == LOW);
   bool f1State = (digitalRead(swFan1) == LOW);
